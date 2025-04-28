2d97e89f4d55be7ab30d3eccb322c14fb8b37c3a
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-28 10:58:40 -0300

    first commit

---
+++ estruturas_basicas_ruby.rb
@@ -0,0 +1,66 @@
+# Print something
+# puts "Ruby"    #prints what's inside the quotation marks and a new line
+# print "Hello, friend"
+# print "same line as Hello, friend"
+
+
+# name = "Marcelo"
+# age = "29"
+# puts "I am #{name} and I am #{age} years old."
+# puts ("I am " + name + " and I am " + age + " years old.")
+
+
+# name = "Marcelo"    #string
+# age = 29            #int
+# index = 0.5         #float
+# ismale = true
+# isclosed = false
+# discount = nil      #no value, absence of value
+
+
+# puts "First line \n next line"
+# puts "Quotation mark\""
+# puts 'Single quotes'
+
+# phrase = "Learn something new everyday"
+# puts phrase
+# puts phrase.upcase()
+# puts phrase.downcase()
+
+# phrase2 = "   The orange is orange.    "
+# puts phrase2.strip()
+# puts phrase2.length()
+# puts phrase2.include? "orange"
+
+# fruit = "Blueberry"
+# puts fruit[0]           #index position starts with 0
+# puts fruit[0,4]
+# puts fruit.index("r")
+
+
+# puts 5
+# puts 2.53254
+# puts -0.4235
+# puts 5 + 11
+# puts 4 - 15
+# puts 1 + 1.2
+# puts 5 * 2
+# puts 5 ** 2
+# puts 5 / 5
+# puts 10 % 2
+# puts 10 % 3
+
+# num = 20
+# puts num
+# puts num.to_s       #number to string
+
+# num = -20.298
+# puts num.abs()
+# puts num.round()
+
+# num = 20.2
+# puts num.ceil()
+# puts num.floor()
+# puts Math.sqrt(36)
+# puts Math.log(1)
+
