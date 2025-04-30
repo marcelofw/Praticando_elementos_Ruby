05ea2609db1ca0cbb08a92b00231ac0772a12605
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-04-30 08:58:21 -0300

    adding file

---
+++ if_statements_ruby.rb
@@ -0,0 +1,52 @@
+# ismale = true
+# if ismale & istall
+#     puts "You are male."
+# else
+#     puts "You are female."
+# end
+
+# isblue = true
+# isround = true
+# if isblue & isround                         #ruby executes both & and "and"
+#     puts "Something is blue and round."
+# end
+# if isblue and isround
+#     puts "Something is blue and round."
+# end
+
+# isred = true
+# isrose = true
+# if isred | isrose                              #same goes for | and "or"
+#     puts "It is red or it is a rose."
+# end
+# if isred or isrose
+#     puts "It is red or it is a rose."
+# end
+
+# ishuman = false
+# if !ishuman
+#     puts "You are not human."
+# end
+
+# ismale = false
+# istall = false
+# if ismale and istall
+#     puts "You are a tall male."
+# elsif ismale and !istall
+#     puts "You are a short male."
+# elsif !ismale and istall
+#     puts "You are not male but are tall."
+# else
+#     puts "You are not male and not tall."
+# end
+
+# def max(num1, num2, num3)
+#     if num1 >= num2 and num1 >= num3
+#         return num1
+#     elsif num2 >= num1 and num2 >= num3
+#         return num2
+#     else
+#         return num3
+#     end
+# end
+# puts max(1, 20, 3)
