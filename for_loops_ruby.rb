bb62b887394b4d7f7a2f0dc8e67ee9eb05796427
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-05-01 08:40:19 -0300

    adding file

---
+++ for_loops_ruby.rb
@@ -0,0 +1,18 @@
+cities = ["Liberty Bay", "Thais", "Carlin", "Port Hope", "Darashia"]
+
+# for city in cities
+#     puts city
+# end
+
+# cities.each do |city|           #another way to iterate through a collection
+#     puts city
+# end
+
+# for index in 0..5
+#     puts index
+# end
+
+6.times do |index|
+    puts index
+end
+
